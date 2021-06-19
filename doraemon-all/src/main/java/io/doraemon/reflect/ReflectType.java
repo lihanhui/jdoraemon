0c40666465eac38e27eb4248654f2dad2c5043c3
wxtsuper <wxtsuper@163.com>
2021-06-19 20:39:17 +0800

    some unused code

--- doraemon-all/src/main/java/io/doraemon/reflect/ReflectType.java
+++ doraemon-all/src/main/java/io/doraemon/reflect/ReflectType.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Type;
 
 import com.google.gson.reflect.TypeToken;
 
-import io.doraemon.restful.ResultMsg;
-
 public class ReflectType {
 	public static <T> Type getType(){
 		return new TypeToken<T>(){}.getType();
